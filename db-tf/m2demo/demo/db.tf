dbd5d7e070e85a9c29c332e1fada246d7b87df3a
dzahirovic <dzahirovic@cds-global.com>
2019-06-21 14:13:17 -0500

    T3 instance test for m2demo environment

--- db-tf/m2demo/demo/db.tf
+++ db-tf/m2demo/demo/db.tf
@@ -7,17 +7,17 @@ resource "aws_db_instance" "basem2" {
   storage_type         = "gp2"
   engine               = "mariadb"
   engine_version       = "10.3.8"
-  instance_class       = "db.t2.medium"
-  name                 = "basem2" #alpha-numberic no signs
+  instance_class       = "db.t3.small"
+  name                 = "m2demo" #alpha-numberic no signs
   username             = "magento"
   password             = "Cd$mage911" #minimum 8 chars
   parameter_group_name = "default.mariadb10.3"
   db_subnet_group_name = "default-vpc-19e3497f"
   skip_final_snapshot = "true"
-  identifier = "basem2"
+  identifier = "m2demo"
 
   tags {
-    Name        = "basem2"
+    Name        = "m2demo"
     Application = "Magento2"
   }
 }
